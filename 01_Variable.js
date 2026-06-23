1dcdd4b20e880ebeef7ed922b53529818a5589b2
PRADEEP321-ai <156075563+PRADEEP321-ai@users.noreply.github.com>
2026-06-23 13:47:29 +0000

    Variable Creation

---
+++ 01_Variable.js
@@ -0,0 +1,14 @@
+const accountId = 12324;
+let accountName = "John Doe";
+var accountPassword = "password123";
+accountCity = "New York";
+
+// accountId = 12;
+
+
+/*
+    Prefer not use var 
+    beacuse of issue in block scope and function scope.
+*/
+
+console.table([accountId, accountName, accountPassword, accountCity])
