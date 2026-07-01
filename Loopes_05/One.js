d6f057799af58dbbc3307a4f4e814e8524c4c28c
PRADEEP321-ai <156075563+PRADEEP321-ai@users.noreply.github.com>
2026-07-01 10:16:48 +0000

    for loop uncomment

--- Loopes_05/One.js
+++ Loopes_05/One.js
@@ -11,41 +11,41 @@
 //     console.log(element);   
 // }
 
-// for(let i = 0; i <= 10; i++){
-//     const element = i;
-//     if(element == 5){
-//         console.log("5 is a element");
-//     }
-//     console.log(element);
-// }
+for(let i = 0; i <= 10; i++){
+    const element = i;
+    if(element == 5){
+        console.log("5 is a element");
+    }
+    console.log(element);
+}
 
-// for(let i = 1; i <= 10; i++){
-//     // console.log(`Outer loop value: ${i}`);
-//     for(let j = 1; j <= 10; j++){
-//         // console.log(`Inner loop value ${j} and outer loop ${i}`);
-//         console.log(i + '*' + j + '=' + i * j);
-//     }
-// }
+for(let i = 1; i <= 10; i++){
+    // console.log(`Outer loop value: ${i}`);
+    for(let j = 1; j <= 10; j++){
+        // console.log(`Inner loop value ${j} and outer loop ${i}`);
+        console.log(i + '*' + j + '=' + i * j);
+    }
+}
 
-// let myArray = [1,2,3,4,5,6,8];
-// console.log(myArray.length);
+let myArray = [1,2,3,4,5,6,8];
+console.log(myArray.length);
 
-// for(let i = 0; i < myArray.length; i++){
-//     const element = myArray[i];
-//     console.log(element);
-// }
+for(let i = 0; i < myArray.length; i++){
+    const element = myArray[i];
+    console.log(element);
+}
 
 //break and continue
 
 
 //break
-// for (let index = 1; index <= 20; index++) {
-//     if(index == 5){
-//         console.log("detected 5");
-//         break;
-//     }
-//     console.log(`value of i is ${index}`);
-// }
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log("detected 5");
+        break;
+    }
+    console.log(`value of i is ${index}`);
+}
 
 //continue
 for (let index = 1; index <= 20; index++) {
